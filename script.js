11cd736c7c917d8607c86387be1029b2bcba2535
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 04:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// Complete the js code
 function Car(make, model) {
-	constructor(make, model){
-		this.make = make;
-		this.model = model;
-	}
-	getMakeModel (){
-		return `${this.make} ${this.model}`
-	}
+    this.make = make;
+    this.model = model;
 }
 
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
 function SportsCar(make, model, topSpeed) {
-	constructor(make, model,topSpeed){
-		this.make = make;
-		this.model = model;
-		this.topspeed = topspeed;
-	}
-	getTopSpeed (){
-		return `${this.topspeed}`
-	}
+    this.make = make;
+    this.model = model;
+    this.topSpeed = topSpeed;
 }
+
+SportsCar.prototype.getTopSpeed = function() {
+    return `${this.topSpeed}`;
+};
 
 // Do not change the code below
 window.Car = Car;
